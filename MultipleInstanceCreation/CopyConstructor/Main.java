24fb1759a7e881e00b6e49517c1808c9b6baa8c0
Pranav <irealpranav@gmail.com>
2026-07-16 21:37:28 +0530

    Demonstrated probelem with copy constructor when it comes to class with children

--- MultipleInstanceCreation/CopyConstructor/Main.java
+++ MultipleInstanceCreation/CopyConstructor/Main.java
@@ -3,17 +3,29 @@ package MultipleInstanceCreation.CopyConstructor;
 public class Main {
     public static void main(String[] args) {
         
-        Email email = new Email("receiver@gmail.com", "sender@gmail.com", "Subject", "Body");
-        Email copyMail = new Email(email);
-
-        System.out.println("Original Email:");
-        email.display();
-        System.out.println("-------Copy Mail-------");
-        copyMail.display();
-        System.out.println("-------End Copy Mail-------");
+        Email email = new PremiumEmail("receiver@gmail.com", "sender@gmail.com", "Subject", "Body", "High");
 
         // Problem comes when we have a child class of the Email Class..
         // There its a confusion, whose copy constructor to call!
+
+        if (email instanceof PremiumEmail) {
+            PremiumEmail premiumEmail = (PremiumEmail) email;
+            PremiumEmail copyPremiumEmail = new PremiumEmail(premiumEmail);
+            System.out.println("Original Premium Email:");
+            premiumEmail.display();
+            System.out.println("-------Copy Premium Email-------");
+            copyPremiumEmail.display();
+            System.out.println("-------End Copy Premium Email-------");
+        }
+
+        else {
+            Email copyEmail = new Email(email);
+            System.out.println("Original Email:");
+            email.display();
+            System.out.println("-------Copy Email-------");
+            copyEmail.display();
+            System.out.println("-------End Copy Email-------");
+        }
 
     }
 }
